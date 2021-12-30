4a3b0f243be453c812e284a980c4741b0896a5d1
finnahern <finna9x@gmail.com>
2021-12-30 23:55:10 +0000

    Add Procfile and prepare settings.py for deployment

--- devsite/devsite/settings.py
+++ devsite/devsite/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,8 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://8000-gray-echidna-7s7rwwax.ws-eu25.gitpod.io']
-CSRF_TRUSTED_ORIGINS = ['https://8000-gray-echidna-7s7rwwax.ws-eu25.gitpod.io']
+ALLOWED_HOSTS = ['https://ms4-django-blog.herokuapp.com', 'localhost', 'https://8000-gray-echidna-7s7rwwax.ws-eu25.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://ms4-django-blog.herokuapp.com', 'https://8000-gray-echidna-7s7rwwax.ws-eu25.gitpod.io']
 
 # Application definition
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'devsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
